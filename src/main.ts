66f4fb86f712d8e906e10ad2edd9acf34801aac3
Anup Sarkar <anupsarkar377@gmail.com>
2018-10-22 11:30:58 +0530

    Remove documentation

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-/**THis is main.ts */
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
