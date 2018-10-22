db797444c0da28f156ad63b1457cf6ca34a9024e
Anup Sarkar <anupsarkar377@gmail.com>
2018-10-22 11:30:03 +0530

    Update total file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { LoginComponent } from './auth/login/login.component';
 import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FirstTimePasswordComponent } from './auth/first-time-password/first-time-password.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FirstTimePasswordComponent,
   ],
   imports: [
     BrowserModule,
